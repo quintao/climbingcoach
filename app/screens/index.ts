30f10f82fed14cdb570536c400bc0f5383bd9a65
Fred Quintao <quintao@gmail.com>
2024-09-06 12:20:54 +0200

    version with bio

---
+++ app/screens/index.ts
@@ -0,0 +1,10 @@
+export * from "./WelcomeScreen"
+
+export * from "./SettingsScreen"
+export * from "./LoginScreen"
+export * from "./DemoDebugScreen"
+export * from "./DemoShowroomScreen/DemoShowroomScreen"
+
+export * from "./ErrorScreen/ErrorBoundary"
+// export other screens here
+export * from "./SettingsScreen"
